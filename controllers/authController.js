f900874d90b44763a6576cceb37b1320fb7597d7
Irfan Mohmand <irfanmohmand987@gmail.com>
2025-05-27 23:55:05 +0500

    switch case api applied

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const { generateAccessToken } = require("../helpers/accessToken");
 const { generateRefreshToken } = require("../helpers/refreshToken");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
   const { username, email, password, accountType } = req.body;
@@ -86,4 +87,99 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { signup, login }
+
+
+// Refresh Token
+const refresh = (req, res) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({
+    success: false,
+    message: "Please login"
+  });
+
+  try {
+
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if (err) return res.status(403).json({
+        success: false,
+        message: err.message
+      });
+
+      const accessToken = generateAccessToken({
+        id: user.id,
+        accountType: user.accountType,
+        author: user.author
+      });
+
+      const refreshToken = generateRefreshToken({
+        id: user.id,
+        accountType: user.accountType,
+        author: user.author
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: "Token refresh successfully.",
+        accessToken,
+        refreshToken,
+        role: user.accountType,
+        author: user.author,
+      });
+
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+
+}
+
+
+
+// Switch Profile
+const switchProfile = async (req, res) => {
+  const authorId = req.id;
+  const authorAccountType = req.accountType;
+  try {
+
+    const user = await User.findByIdAndUpdate(authorId, {
+      accountType: authorAccountType === "buyer" ? "seller" : "buyer"
+    });
+
+    if (!user) return res.status(404).json({
+      success: false,
+      message: "User not found."
+    });
+
+    const data = {
+      id: user._id,
+      accountType: user.accountType,
+      author: user.author
+    };
+
+    const accessToken = generateAccessToken(data);
+    const refreshToken = generateRefreshToken(data);
+
+    return res.status(200).json({
+      success: true,
+      message: `Switched to ${user.accountType}`,
+      accessToken,
+      refreshToken,
+      role: user.accountType,
+      author: user.username
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
+
+module.exports = { signup, login, refresh, switchProfile }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,11 @@
-const { login, signup } = require('../controllers/authController');
+const { login, signup, refresh, switchProfile } = require('../controllers/authController');
 
 const router = require('express').Router();
+const { verifyToken } = require('../middlewares/verifyToken');
 
 router.post("/login", login);
 router.post("/signup", signup);
+router.get("/refresh", refresh);
+router.get("/switch", verifyToken, switchProfile);
 
 module.exports = router;
